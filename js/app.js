c1c9dd542de904c957cf374b64756d3fee9876af
aplaraujo <aplaraujo@gmail.com>
2022-12-05 20:34:47 -0300

    Refatoração do código JS

--- js/app.js
+++ js/app.js
@@ -1,40 +1,59 @@
-const form = document.querySelector('form')
-const result = document.querySelector('.result')
-const showPopup = document.querySelector('.quiz-popup')
-const popupClose = document.querySelector('.quiz-popup__close')
-const correctAnswers = ['C', 'C', 'C', 'A', 'B']
+const form = document.querySelector("form");
+const result = document.querySelector(".result");
+const showPopup = document.querySelector(".quiz-popup");
+const popupClose = document.querySelector(".quiz-popup__close");
+const correctAnswers = ["C", "C", "C", "A", "B"];
 
-form.addEventListener('submit', event => {
-    event.preventDefault()
-    showPopup.style.display = 'block'
-    let score = 0
-    const userAnswers = [
-        form.inputQuestion1.value,
-        form.inputQuestion2.value,
-        form.inputQuestion3.value,
-        form.inputQuestion4.value,
-        form.inputQuestion5.value,
-    ]
-    userAnswers.forEach((answer, index) => {
-        if(answer === correctAnswers[index]) {
-            score += 20
-        }
-    })
-    console.log(score)
+let score = 0;
 
-    let counter = 0
+const getUserAnswers = () => {
+  let userAnswers = [];
+  correctAnswers.forEach((_, index) => {
+    const userAnswer = form[`inputQuestion${index + 1}`].value;
+    userAnswers.push(userAnswer);
+  });
+  return userAnswers;
+};
 
-    const timer = setInterval(() => {
-        if(counter === score) {
-            clearInterval(timer)
-        }
-        result.querySelector('span').textContent = `${counter}%`
-        counter++
-    }, 10)
-})
+const calculateUserScore = (userAnswers) => {
+  userAnswers.forEach((answer, index) => {
+    const isTheUserAnswerCorrect = answer === correctAnswers[index];
+    if (isTheUserAnswerCorrect) {
+      score += 20;
+    }
+  });
+};
 
-popupClose.addEventListener('click', () => {
-    showPopup.style.display = 'none'
-    scrollTo(0, 0)
-    form.reset()
-})
+const scrollScreen = () => {
+  scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+
+const animateFinalScore = () => {
+  let counter = 0;
+
+  const timer = setInterval(() => {
+    if (counter === score) {
+      clearInterval(timer);
+    }
+    result.querySelector("span").textContent = `${counter}%`;
+    counter++;
+  }, 10);
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  showPopup.style.display = "block";
+  const userAnswers = getUserAnswers()
+  calculateUserScore(userAnswers)
+  animateFinalScore()
+});
+
+popupClose.addEventListener("click", () => {
+  showPopup.style.display = "none";
+  scrollScreen()
+  form.reset();
+});
